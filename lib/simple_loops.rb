1dd18e531622f2682624ae1b9af7a07d56637e7c
Victor Cordero <victordcordero@gmail.com>
2020-10-20 00:45:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -32,3 +32,13 @@ def return_string_array(array)
     counter += 1
   end
   end
+
+  def return_string_array(array)
+  count = 0
+  new_array = []
+  while count < array.length do
+    new_array.push(array[count].to_s) # OR new_array << array[count].to_s
+    count += 1
+  end
+  new_array
+end
